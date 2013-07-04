430ff361dfb4a147467c787ce13d80c17e313c13
ankitsh <ankit.sharma@iic.ac.in>
2013-07-04 17:12:28 +0530

    Third One

---
+++ Breastfeeding.mm
@@ -0,0 +1,45 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1372060699165" ID="ID_1750511313" MODIFIED="1372063056806" TEXT="Breast Feeding(Data collection App)">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1372060929588" FOLDED="true" ID="ID_1725805970" MODIFIED="1372063543472" POSITION="left" TEXT="Data collecting module">
+<icon BUILTIN="full-2"/>
+<node CREATED="1372061803034" ID="ID_1243807022" MODIFIED="1372063312959" TEXT="fetching the contact numbers from the Database.">
+<node CREATED="1372061884973" ID="ID_1600613967" MODIFIED="1372061966352" TEXT="setup an sms server">
+<node CREATED="1372062009393" ID="ID_64578200" MODIFIED="1372063381410" TEXT="if contact no is mobile send SMS based simple questions.">
+<cloud/>
+<node CREATED="1372062208298" ID="ID_183683643" MODIFIED="1372063454532" TEXT="store the data">
+<cloud/>
+</node>
+</node>
+</node>
+<node CREATED="1372061972742" ID="ID_624175493" MODIFIED="1372063534245" TEXT="setup an IVRS system">
+<node CREATED="1372062090532" ID="ID_1218122109" MODIFIED="1372063384673" TEXT="if contact number is landline contact via IVRS">
+<cloud/>
+<node CREATED="1372062225211" ID="ID_1525223964" MODIFIED="1372063339630" TEXT="store the data">
+<cloud/>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1372060982429" FOLDED="true" ID="ID_1365242993" MODIFIED="1372063925703" POSITION="right" TEXT="Analysis/visualization module">
+<icon BUILTIN="full-3"/>
+<node CREATED="1372062536417" ID="ID_1765322728" MODIFIED="1372063121288" TEXT="conclusions can be given to provide tips for improving the breast feeding in India to reduce malnutrition">
+<cloud/>
+</node>
+</node>
+<node CREATED="1372061118642" FOLDED="true" ID="ID_1022055858" MODIFIED="1372063923438" POSITION="right" TEXT="Registration Module">
+<icon BUILTIN="full-1"/>
+<node CREATED="1372061252925" ID="ID_1825589207" MODIFIED="1372063111593" TEXT="Creating a web based form for registration either by the hospital staff/family member to get the basic info about the mother &amp; the baby">
+<cloud/>
+</node>
+<node CREATED="1372061559485" ID="ID_190851140" MODIFIED="1372063077550" TEXT="Designing a Data Base server to collect the Data including the basic info.">
+<cloud/>
+</node>
+<node CREATED="1372061648864" ID="ID_1417634867" MODIFIED="1372063126965" TEXT="Linking the web based form with the database.">
+<cloud/>
+</node>
+</node>
+</node>
+</map>
